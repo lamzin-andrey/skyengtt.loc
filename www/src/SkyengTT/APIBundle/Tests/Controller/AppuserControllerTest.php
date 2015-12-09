ff92e8b3b75bb99d7d658cec5fd3f3b4ba646a1f
andrey <andrey@andrey-NULL>
2015-12-09 16:40:05 +0300

    Backend Функциональный тест создания игровой сессии пользователей

--- www/src/SkyengTT/APIBundle/Tests/Controller/AppuserControllerTest.php
+++ www/src/SkyengTT/APIBundle/Tests/Controller/AppuserControllerTest.php
@@ -14,6 +14,50 @@ class AppuserController extends WebTestCase
 {
 	public function testCreateAction()
 	{
+		$this->_createUser($jsonResponseData, $repository, $username, $em);
+        $data = $jsonResponseData;
+        $this->assertTrue((int)$data->id > 0);
+        $list = $repository->findBy(array('name' => $username));
+        $this->assertTrue(count($list) == 1);
+        $user = $list[0];
+        $this->assertTrue($user->getId() == $data->id);
+        $this->_deleteUser($em, $data->id);
+	}
+	
+	public function testCreateSession()
+	{
+		//Пусть сразу вошли на /#/game  и пытаемся отправить ответ
+		//(для простоты тестирования есть тестовая форма /answer)
+		$this->_get('/answer', $client, $crawler, $html, $json);
+		$form = $crawler->selectButton('Send')->form(array(
+            'answer_id' => 4,
+            'quest_id'  => 5
+        ));
+        $client->submit($form);
+        $response = $client->getResponse();
+        $jsonResponseData = json_decode($response->getContent());
+        $this->assertTrue($jsonResponseData->info == 'Lost session');
+        
+        //А теперь сначала создадим пользователя
+		$this->_createUser($userJson, $repository, $username, $em, $client);
+		
+		//Пробуем ответить на вопрос
+		$this->_get('/answer', $client, $crawler, $html, $json);
+		$form = $crawler->selectButton('Send')->form(array(
+            'answer_id' => 4,
+            'quest_id'  => 5
+        ));
+        $client->submit($form);
+        $response = $client->getResponse();
+        $jsonResponseData = json_decode($response->getContent());
+        $this->assertTrue(@$jsonResponseData->info != 'Lost session');
+        $this->assertTrue(@$jsonResponseData->info == 'Answer #4 not found!');
+        $this->_deleteUser($em, $userJson->id);
+	}
+	
+	
+	private function _createUser(&$jsonResponseData, &$repository, &$username, &$em, &$client = null)
+    {
 		$username = md5('angula_simfony_test_user'. time());
 		$client = static::createClient();
 		$this->_getEnvParam($client, $em, $repository, 'SkyengTT\SkyengTTBundle\Entity\AnonymousUsers');
@@ -26,15 +70,17 @@ class AppuserController extends WebTestCase
         $client->submit($form);
         
         $response = $client->getResponse();
-        $data = json_decode($response->getContent());
-        $this->assertTrue((int)$data->id > 0);
-        
-        $list = $repository->findBy(array('name' => $username));
-        $this->assertTrue(count($list) == 1);
-        $user = $list[0];
-        $this->assertTrue($user->getId() == $data->id);
-        $this->_deleteUser($em, $data->id);
-        
+        $jsonResponseData = json_decode($response->getContent());
+	}
+	
+	private function _get($url, &$client = null, &$crawler, &$responseText, &$jsonResponse) {
+		if (!$client) {
+			$client = static::createClient();
+		}
+		$crawler = $client->request('GET', $url);
+		$response = $client->getResponse();
+		$responseText = $response->getContent();
+		$jsonResponse = json_decode($responseText);
 	}
 	
 	private function _deleteUser($em, $id)
