6c78164d9e84e8309f70befc06d4a82c5ddb4761
andrey <andrey@andrey-NULL>
2015-12-09 18:23:40 +0300

    Backend Реализованы два первых теста для контроллера ответов

--- www/src/SkyengTT/APIBundle/Tests/Controller/AnswerControllerTest.php
+++ www/src/SkyengTT/APIBundle/Tests/Controller/AnswerControllerTest.php
@@ -16,13 +16,11 @@ use \SkyengTT\SkyengTTBundle\Utils\TestLib AS U;
 
 use \SkyengTT\SkyengTTBundle\Utils\AppTestLib AS ATL;
 
+use Symfony\Component\DomCrawler\Form AS CForm;
+
 class AnswerController extends WebTestCase
 {
-	public function testaddAnswer()
-	{
-		/**
-			Пытаемся ответить на вопрос неправильно.
-			Пытаемся ответить на вопрос правильно.
+	/**
 			Отвечаем на вопрос неправильно еще дважды, убеждаемся что gameover
 
 			Пытаемся ответить на несуществующий вопрос несуществующим ответом.
@@ -34,13 +32,70 @@ class AnswerController extends WebTestCase
 			Создаем спец вопрос, отвечаем на него неправильно Random раз
 			Убеждаемся, что в статистике будет ровно Random для этого вопроса
    
-		 * */
+	*/
+	/**
+	 * Пытаемся ответить на вопрос неправильно.
+	 * (Пробуем ответить на существующий вопрос существующим неправильным ответом)
+	*/
+	public function testaddWrongAnswer()
+	{
+		ATL::createUser($userJson, $r, $name, $em, $client);
+		//Пробуем ответить на существующий вопрос существующим неправильным ответом
+		$questions = $this->_getExistsQuestions();
+		$this->assertTrue(count($questions) > 1);
+		$question = $questions[0];
+		$questionForAnswer = $questions[1];
+		
+        $json = $this->_sendAnswer($questionForAnswer->getAnswerId(), $question->getId(), $client);
+        $this->assertFalse($json->success);
+        
+        ATL::deleteUser($em, $userJson->id);
+	}
+	/**
+	 * Пытаемся ответить на вопрос правильно.
+	*/
+	public function testaddRightAnswer()
+	{
+		ATL::createUser($userJson, $r, $name, $em, $client);
+		//Пробуем ответить на существующий вопрос существующим неправильным ответом
+		$questions = $this->_getExistsQuestions();
+		$this->assertTrue(count($questions) > 1);
+		$question = $questions[0];
+		
+        $json = $this->_sendAnswer($question->getAnswerId(), $question->getId(), $client);
+        $this->assertTrue($json->success);
+        $this->assertTrue($json->score > 0);
+        ATL::assertNextQuestionIsPresent($json, $this);
+        ATL::deleteUser($em, $userJson->id);
 	}
 	
+	private function _sendAnswer($answerId, $questionId, $client, &$text = null)
+	{
+		U::get('/answer', $client, $crawler, $html, $json);
+		$form = $crawler->selectButton('Send')->form(array(
+            'answer_id' => strval($answerId),
+            'quest_id'  => $questionId
+        ));
+        
+        /*
+         * Попытка добратья до приватного свойства формы, по сути успешная, но сессия теряется...
+         * $reflectionClass = new \ReflectionClass('Symfony\Component\DomCrawler\Form');
+		$prop = $reflectionClass->getProperty('currentUri');
+		$prop->setAccessible(true);
+		$prop->setValue($form, 'http://ang.loc:8000/answer/add/' . $questionId);*/
+		
+        $client->submit($form);
+        $response = $client->getResponse();
+        $text = $response->getContent();
+        $json = json_decode($text);
+        return $json;
+	}
 	
-	
-	
-	
-	
+	private function _getExistsQuestions()
+	{
+		$client = static::createClient();
+		U::getEnvParam($client, $em, $r, 'SkyengTT\SkyengTTBundle\Entity\Vocabulary');
+		return $r->findBy([], ['id' => 'ASC'], 10, 0);
+	}
 }
 
--- www/src/SkyengTT/SkyengTTBundle/Utils/AppTestLib.php
+++ www/src/SkyengTT/SkyengTTBundle/Utils/AppTestLib.php
@@ -28,4 +28,34 @@ class AppTestLib extends TestLib
 		$em->createQuery("DELETE FROM SkyengTT\SkyengTTBundle\Entity\AnonymousUsers AS u WHERE u.id = {$id}")
         ->getSingleResult();
 	}
+	
+	static public function assertNextQuestionIsPresent($json, $_this)
+	{
+		$_this->assertTrue( isset($json->next) );
+        $_this->assertTrue( isset($json->next->answers) );
+        $_this->assertTrue( isset($json->next->question) );
+        $_this->assertTrue( isset($json->next->question->id) );
+        $_this->assertTrue( isset($json->next->question->word) );
+        $_this->assertTrue( is_array($json->next->answers) );
+        //Убеждаемся, что все ответы имеют все необходимые поля
+        //и что среди них есть ответ на вопрос
+        $rightAnswerId = self::_getRightAnswerId($json->next->question->id);
+        $trueVariantPresent = false;
+        foreach ($json->next->answers as $answer) {
+			$_this->assertTrue( isset($answer->id) );
+			$_this->assertTrue( isset($answer->word) );
+			if ($answer->id == $rightAnswerId) {
+				$rightAnswerPresent = true;
+			}
+		}
+		$_this->assertTrue($rightAnswerPresent);
+	}
+	
+	static private function _getRightAnswerId($questionId)
+	{
+		$client = static::createClient();
+		self::getEnvParam($client, $em, $r, 'SkyengTT\SkyengTTBundle\Entity\Vocabulary');
+		$question = $r->find($questionId);
+		return $question->getAnswerId();
+	}
 }
